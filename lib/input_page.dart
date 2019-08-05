29b6f2ef2637357eb53a33857ec0eb9d4aad62fb
Mubanga <mubangeriseme@yahoo.com>
2019-08-05 22:12:41 +0200

    BMI Calculator: Male & Female Icon Detail Cards Created

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const BOTTOM_CONTAINER_HEIGHT = 80.0;
 const COLOUR_BOTTOM_CONTAINER = Color(0xFFEB1555);
 const COLOUR_DETAIL_CARD = Color(0xFF1D1E33);
+const COLOUR_TEXT_DETAIL_CARD = Color(0xFF8D8E98);
 
 class InputPage extends StatefulWidget {
   @override
@@ -19,10 +21,43 @@ class _InputPageState extends State<InputPage> {
       ),
       body: Column(
         children: <Widget>[
-          Expanded(flex: 3, child: MultipleDetailCards(2)),
+          Expanded(
+              flex: 3,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                      flex: 2,
+                      child: SingleDetailCard(
+                        colour: COLOUR_DETAIL_CARD,
+                        cardChild: IconDetail(
+                          displayIcon: FontAwesomeIcons.mars,
+                          descriptionText: "MALE",
+                        ),
+                      )),
+                  Expanded(
+                      flex: 2,
+                      child: SingleDetailCard(
+                        colour: COLOUR_DETAIL_CARD,
+                        cardChild: IconDetail(
+                            displayIcon: FontAwesomeIcons.venus,
+                            descriptionText: "FEMALE"),
+                      ))
+                ],
+              )),
           Expanded(
               flex: 3, child: SingleDetailCard(colour: COLOUR_DETAIL_CARD)),
-          Expanded(flex: 3, child: MultipleDetailCards(2)),
+          Expanded(
+              flex: 3,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                      flex: 2,
+                      child: SingleDetailCard(colour: COLOUR_DETAIL_CARD)),
+                  Expanded(
+                      flex: 2,
+                      child: SingleDetailCard(colour: COLOUR_DETAIL_CARD))
+                ],
+              )),
           Container(
             color: COLOUR_BOTTOM_CONTAINER,
             margin: EdgeInsets.only(top: 10.0),
@@ -46,6 +81,33 @@ class _InputPageState extends State<InputPage> {
       child: Row(
         children: _NumberOfCards,
       ),
+    );
+  }
+}
+
+class IconDetail extends StatelessWidget {
+  final String descriptionText;
+  final IconData displayIcon;
+
+  IconDetail({@required this.displayIcon, @required this.descriptionText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          displayIcon,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          descriptionText,
+          style: TextStyle(color: COLOUR_TEXT_DETAIL_CARD, fontSize: 18.00),
+        )
+      ],
     );
   }
 }
