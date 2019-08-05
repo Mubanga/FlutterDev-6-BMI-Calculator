bcbeaff0158dc21760078c06f570c5b9f3fe872c
Mubanga <mubangeriseme@yahoo.com>
2019-08-05 21:22:55 +0200

    BMI Calculator: Themes Const Used For Colours

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+const BOTTOM_CONTAINER_HEIGHT = 80.0;
+const COLOUR_BOTTOM_CONTAINER = Color(0xFFEB1555);
+const COLOUR_DETAIL_CARD = Color(0xFF1D1E33);
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -16,42 +20,33 @@ class _InputPageState extends State<InputPage> {
       body: Column(
         children: <Widget>[
           Expanded(flex: 3, child: MultipleDetailCards(2)),
-          Expanded(flex: 3, child: SingleDetailCard()),
-          Expanded(flex: 3, child: MultipleDetailCards(2))
-        ],
-      ),
-    );
-  }
-
-  Container SingleDetailCard() {
-    return Container(
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-          color: Color(0xFF1D1E33), borderRadius: BorderRadius.circular(10.00)),
-    );
-  }
-
-  Container TwoDetailCard() {
-    return Container(
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            flex: 2,
-            child: SingleDetailCard(),
-          ),
-          Expanded(
-            flex: 2,
-            child: SingleDetailCard(),
+          Expanded(flex: 3, child: SingleDetailCard(COLOUR_DETAIL_CARD)),
+          Expanded(flex: 3, child: MultipleDetailCards(2)),
+          Container(
+            color: COLOUR_BOTTOM_CONTAINER,
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: BOTTOM_CONTAINER_HEIGHT,
           )
         ],
       ),
     );
   }
 
+  Container SingleDetailCard(Color colour) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+          color: colour, borderRadius: BorderRadius.circular(10.00)),
+    );
+  }
+
+
   Container MultipleDetailCards(int CardNumber) {
     List<Expanded> _NumberOfCards = [];
     for (int x = 0; x < CardNumber; x++) {
-      Expanded card = Expanded(flex: CardNumber, child: SingleDetailCard());
+      Expanded card = Expanded(
+          flex: CardNumber, child: SingleDetailCard(COLOUR_DETAIL_CARD));
       _NumberOfCards.add(card);
     }
     return Container(
